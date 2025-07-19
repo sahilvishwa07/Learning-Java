89bfd56d7feb8469d4f453ea95c267d7dec37ffb
Sahil1246 <125081449+Sahil1246@users.noreply.github.com>
2025-07-19 18:57:25 +0530

    linked list covered

--- Conditional/Practice.java
+++ Conditional/Practice.java
@@ -18,11 +18,11 @@ public class Practice {
         
         /*Finish the following code so that it prints You have a fever if your temperature
         is above 100 and otherwise prints You don't have a fever*/
-        double temp = 103.5;
-        if(temp>=100){
-            System.out.println("you have fever");
-        }else{
-            System.out.println("you dont have fever");
-        }
+        // double temp = 103.5;
+        // if(temp>=100){
+        //     System.out.println("you have fever");
+        // }else{
+        //     System.out.println("you dont have fever");
+        // }
     }
 }
