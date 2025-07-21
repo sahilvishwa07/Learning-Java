57ed86c584f2e5069228e12563e9075dd93443bc
sahilvishwa07 <sahilvishwakarma711@gmail.com>
2025-07-21 23:21:28 +0530

    Pattern Print

--- Loops/Prime.java
+++ Loops/Prime.java
@@ -3,13 +3,19 @@ package Loops;
 import java.util.Scanner;
 
 public class Prime {
+    public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
+    System.out.print("Enter a number: ");
     int n = sc.nextInt();
 
-    boolean isPrime = true;
-    for(int i=2;i<=n-1;i++){
-        if(n%i ==0){
+    if(n == 2){
+        System.out.println("n is prime");
+    }else{
+        boolean isPrime = true;
+        for(int i=2;i<=Math.sqrt(n);i++){
+            if(n%i ==0){
             isPrime = false;
+            }
         }
         if(isPrime == true){
             System.out.println("n is Prime ");
@@ -18,4 +24,6 @@ public class Prime {
             System.out.println("n is not Prime");
         }
     }
+    }
 }
+
