916850ddaf5a62a2423ff1360c229a1b6446d8b0
sahilvishwa07 <sahilvishwakarma711@gmail.com>
2025-07-30 17:54:48 +0530

    Array of brute force

--- Array/MaxSubarraySum.java
+++ Array/MaxSubarraySum.java
@@ -57,11 +57,20 @@ public class MaxSubarraySum {
     // }
 
     public static void Kadane(int numbers[]){
-        
+        int ms = Integer.MIN_VALUE;
+        int cs = 0;
+        for (int i=0; i<numbers.length; i++){
+            cs = cs + numbers[i];
+            if(cs < 0) {
+                cs = 0;
+            }
+            ms = Math.max(cs, ms);
+        }
+        System.out.println("Our Max Sub Array Sum is : " + ms);
 
     }
     public static void main(String[] args) {
-        int numbers[] = {1, -2, 6 ,-1 ,8 ,3};  //{2, 4, 6, 8, 10};
+        int numbers[] = {-2, -3 ,4 ,-1 ,-2 ,1 ,5 ,-3};  //{2, 4, 6, 8, 10};
         Kadane(numbers);
     }
     
